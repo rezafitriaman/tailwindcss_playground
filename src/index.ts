f646df3b87f9218a3c6378c64b99f50c25bd9aa1
Reza Fitriaman <reza@canvascompany.nl>
2021-07-29 00:00:34 +0200

    add main menu

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,6 @@ import AlpineCode from 'alpinejs'
 
 AlpineCode.start();
 
-AlpineCode.data('dropdown', () => ({
-    open: false,
-
-    toggle() {
-        this.open = ! this.open
-    }
-}))
-
-
-
-/*import './index.phtml';*/
 /*import { TextField } from './ts/textField/index';
 
 new TextField().init();*/
